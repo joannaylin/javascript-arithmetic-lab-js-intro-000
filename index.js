c653fd5a9c4b9fa688e5606bd58ee143b7f60c28
Joanna Lin <lin.joanna.y@gmail.com>
2020-02-11 15:20:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,15 +10,5 @@ function multiply(a,b) {
 function divide(a,b) {
   return a/b
 }
-function increment(n) {
+function increment(n)
   return n+=
-}
-function decrement(n) {
-  return n-=
-}
-function makeInt(n) {
-  return parseInt(n,10)
-}
-function preserveDecimal(n) {
-  return parseFloat(n)
-}
