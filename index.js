c4cfcf64513515fb48702cc4bacc047fe4c25d5e
Joanna Lin <lin.joanna.y@gmail.com>
2020-02-11 15:19:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,4 +7,18 @@ function subtract(a,b) {
 function multiply(a,b) {
   return a*b
 }
-divide()
+function divide(a,b) {
+  return a/b
+}
+function increment(n) {
+  return n+=
+}
+function decrement(n) {
+  return n-=
+}
+function makeInt(n) {
+  return parseInt(n,10)
+}
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
